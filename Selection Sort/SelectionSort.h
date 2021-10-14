2126cc44589f03b10022bf13470bbe82585e0df3
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-14 11:34:16 -0300

    adds .h file in selection sort

---
+++ Selection Sort/SelectionSort.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct repositoriodeprodutos Repo;
+typedef struct produto Prod;
+
+Prod* criaProduto(int cod, float val);
+
+
+Repo* criarRepositorio(int n);
+
+int adicionar(Repo* re, int cod, float val);
+
+void selectionSort(Repo* re);
+
+void esvaziar(Repo* re);
+
+void printRespositorio(Repo* re);
+
