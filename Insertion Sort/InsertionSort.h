b132a1b83c83a08898797840fdf3829045537d0f
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-14 11:24:45 -0300

    adds .h file in insertion sort

---
+++ Insertion Sort/InsertionSort.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct repositoriodeprodutos Repo;
+typedef struct produto Prod;
+
+Prod* criaProduto(int cod, float val);
+
+Repo* criarRepositorio(int n);
+
+int adicionar(Repo* re, int cod, float val);
+
+void insertionSort(Repo* re);
+
+void esvaziar(Repo* re);
+
+void printRespositorio(Repo* re);
+
