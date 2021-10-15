2fc5b3edf1c5550c1cb575fa41895a39d9299b84
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-15 10:44:46 -0300

    adds main.c in bubble sort

---
+++ Bubble Sort/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BubbleSort.h"
+
+int main(){
+	
+	Repo* re = criarRepositorio(5);
+			
+	adicionar(re, 555, 600);
+	adicionar(re, 111, 10);
+	adicionar(re, 444, 499);
+	adicionar(re, 333, 150);
+	adicionar(re, 222, 120);
+	
+	printRespositorio(re);
+	
+	bubbleSort(re);
+	
+	printRespositorio(re);
+	
+	esvaziar(re);
+	
+}
+
+
