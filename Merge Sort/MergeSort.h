bf77c47706d51c66fc3516210a471ad742abcdb5
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-16 12:26:49 -0300

    adds .h file in merge sort

---
+++ Merge Sort/MergeSort.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct repositoriodeprodutos Repo;
+typedef struct produto Prod;
+
+Prod* criaProduto(int cod, float val);
+
+
+Repo* criarRepositorio(int n);
+
+int adicionar(Repo* re, int cod, float val);
+
+void mergeSort(Repo* re, int ini, int fim);
+
+void mergeSortaux(Repo* re, int ini, int mei, int fim);
+
+void esvaziar(Repo* re);
+
+void printRespositorio(Repo* re);
+
