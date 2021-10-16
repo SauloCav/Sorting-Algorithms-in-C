fe787b08c47389b10e1b3934e525241c823827bd
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-16 12:25:01 -0300

    adds main.c in merge sort

---
+++ Merge Sort/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "MergeSort.h"
+
+int main(){
+	
+	int n = 5;
+	
+	Repo* re = criarRepositorio(n);
+			
+	adicionar(re, 555, 600);
+	adicionar(re, 111, 10);
+	adicionar(re, 444, 499);
+	adicionar(re, 333, 150);
+	adicionar(re, 222, 120);
+	
+	printRespositorio(re);
+	
+	mergeSort(re, 0, n-1);
+	
+	printRespositorio(re);
+	
+	esvaziar(re);
+	
+}
+
+
