260922ff4ee0cf603581788678e2227db69058b4
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-10-18 09:13:02 -0300

    adds .h file in quick sort

---
+++ Quick Sort/QuickSort.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct repositoriodeprodutos Repo;
+typedef struct produto Prod;
+
+Prod* criaProduto(int cod, float val);
+
+
+Repo* criarRepositorio(int n);
+
+int adicionar(Repo* re, int cod, float val);
+
+void quickSort(Repo* re, int ini, int fim);
+
+void esvaziar(Repo* re);
+
+void printRespositorio(Repo* re);
+
