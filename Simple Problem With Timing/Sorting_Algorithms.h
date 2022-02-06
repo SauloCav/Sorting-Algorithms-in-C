8b58a7200d315666ac5913568d615150c61cee8f
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-02-06 10:38:39 -0300

    Add files via upload

---
+++ Simple Problem With Timing/Sorting_Algorithms.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int TAM, int *vetor);
+
+void quickSort(int TAM, int *vetor, int ini, int fim);
+
+void mergeSort(int TAM, int *vetor, int ini, int fim);
+void mergeSortaux(int TAM, int *vetor, int ini, int mei, int fim);
+
+void selectionSort(int TAM, int *vetor);
+
